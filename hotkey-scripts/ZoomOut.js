f50bdd24027d53df2e4d08044399868bf11ca18e
cwest <bokthaku@gmail.com>
2022-12-29 15:53:28 -0500

    Add Zoom In/Out scripts

---
+++ hotkey-scripts/ZoomOut.js
@@ -0,0 +1,26 @@
+// zooms in (normally only possible with mousewheel)
+var SCRIPT_TITLE = "Zoom Out";
+
+// how much to zoom per button press (as a percent)
+var ZOOM_FACTOR = 0.2;
+
+function getClientInfo() {
+  return {
+    "name": SV.T(SCRIPT_TITLE),
+    "category": "Claire's Scripts - Hotkey Scripts",
+    "author": "https://github.com/claire-west/svstudio-scripts",
+    "versionNumber": 1,
+    "minEditorVersion": 65537
+  }
+}
+
+function zoomViewport() {
+  var viewport = SV.getMainEditor().getNavigation();
+  var currentScale = viewport.getTimePxPerUnit();
+  viewport.setTimeScale(currentScale - (currentScale * ZOOM_FACTOR));
+}
+
+function main() {
+  zoomViewport();
+  SV.finish();
+}
